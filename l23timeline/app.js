409d63e23193d04f4495e18b458a96d9b0700602
ThoonShweYiKyaw <thoonlay779@gmai.com>
2022-12-19 19:29:27 +0630

    19DEC

---
+++ l23timeline/app.js
@@ -0,0 +1,15 @@
+var getboxes = document.querySelectorAll('.boxes');
+
+for(var x= 0;x<getboxes.length; x++){
+    getboxes[x].addEventListener('mouseenter',function(){
+    //    console.log(this); 
+
+        if(this.classList.contains("left")){
+            this.classList.remove("left");
+            this.classList.add("right");
+        }else{
+            this.classList.remove("right");
+            this.classList.toggle("left");
+        }
+    });
+}
